6408dc6c9665b1296ba8f0bc697f2bdcbe2fff7d
Alexander Kuziv <makklays@gmail.com>
2021-11-28 21:01:11 +0100

    Create app-todo.js

---
+++ js/app-todo.js
@@ -0,0 +1,39 @@
+const todos = [
+    {id: 1, title: "todo1", done: false},
+    {id: 2, title: "todo2", done: true},
+    {id: 3, title: "todo3", done: false},
+    {id: 4, title: "todo4", done: true},
+    {id: 5, title: "todo5", done: false},
+    {id: 6, title: "todo6", done: true},
+    {id: 7, title: "todo7", done: true},
+];
+
+Vue.component('todo-list', {
+    props: ['id', 'title', 'done', 'item'],
+    template: `<div class="todo-item">
+                 <div class="item-id">#{{ id }}</div>
+                 <div class="">
+                    <input type="text" v-bind:name="id" v-bind:value="title" class="item-title form-control" />
+                 </div>
+                 <div class="form-check form-switch">
+                    <input type="checkbox" v-bind:checked="done" role="switch" class="item-check form-check-input" />
+                 </div>
+               </div>`
+});
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        todos: todos /*[
+            {id: 1, title: "todo1", done: false},
+            {id: 2, title: "todo2", done: true},
+            {id: 3, title: "todo3", done: false},
+            {id: 4, title: "todo4", done: true},
+            {id: 5, title: "todo5", done: false},
+            {id: 6, title: "todo6", done: true},
+            {id: 7, title: "todo7", done: true},
+        ]*/
+    }
+});
+
+
