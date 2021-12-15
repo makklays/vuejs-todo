8cc6647778952f1d682d1986f2d04892cf87f3c9
Alexander Kuziv <makklays@gmail.com>
2021-12-15 12:27:27 +0100

    Create web.php

---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+Route::get('/', function() {
+  $ip       = $_SERVER['REMOTE_ADDR'];
+  $referer  = $_SERVER['HTTP_REFERER'];
+  $agent    = $_SERVER['HTTP_USER_AGENT'];
+  
+  return view('welcome', [
+    'ip' => $ip,
+    'referer' => $referer,
+    'agent' => $agent,
+  ]);
+});
+
+Route::post('/form-todo-submit', 'FormTodoController@submit');
