0cd138d834a8a1b076c33e33dbbd2ffc07d47e21
Alexander Kuziv <makklays@gmail.com>
2021-12-15 12:06:32 +0100

    Create FormTodo.vue

---
+++ js/components/FormTodo.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fields: {},
+      errors: {},
+    }
+  },
+  methods: {
+    submit() {
+      this.errors = {};
+      axios.post('/vue-todo-submit', this.fields).then(response => {
+        alert('Nessage sent!');
+      }).catch(error => {
+        if (error.response.status === 422) {
+          this.errors = error.response.data.errors || {};
+        }
+      });
+    },
+  },
+}
+</script>
