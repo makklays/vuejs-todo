3264b3b0d7375ddae7c73641761a54e3a6bb5d71
Alexander Kuziv <makklays@gmail.com>
2022-03-22 19:26:58 +0100

    Create FileUploadComponent.vue

---
+++ js/components/FileUploadComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <form enctype="multipart/form-data" novalidate v-if="isInitial || isSaving">
+    <h1>Upload images</h1>
+    <div class="dropbox">
+      <input type="file" multiple :name="uploadFieldName" :disabled="isSaving" @change="filesChange($event.target.name, $event.target.files); fileCount = $event.target.files.length"
+      accept="image/*" class="">
+      
+      
+    </div>
+  </form>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
