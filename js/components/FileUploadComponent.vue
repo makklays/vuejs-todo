840033f985865f7bc1786c6a354877dce5b3712a
Alexander Kuziv <makklays@gmail.com>
2022-03-22 19:38:37 +0100

    Update FileUploadComponent.vue

--- js/components/FileUploadComponent.vue
+++ js/components/FileUploadComponent.vue
@@ -20,6 +20,34 @@
 
 </script>
 
-<style>
+<style lang="scss">
+.dropbox {
+  outline: 2px dashed grey; /* the dash box */
+  outline-offset: -10px;
+  background: lightcyan;
+  color: dimgray;
+  padding: 10px 10px;
+  min-height: 200px; 
+  position: relative;
+  cursor: pointer;
+}
 
+.input-file {
+  opacity: 0;
+  width: 100%;
+  height: 200px;
+  position: absolute;
+  cursor: pointer;
+}
+
+.dropbox:hover {
+  background: litghblue;
+}
+
+.dropbox p {
+  font-size: 1.2em;
+  text-align: center;
+  padding: 50px 0;
+}
 </style>
+
