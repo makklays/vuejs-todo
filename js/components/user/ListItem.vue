4ea3a0613b21d45c2ecdc09a8b0b094b6507ece8
Alexander Kuziv <makklays@gmail.com>
2022-03-22 15:45:17 +0100

    Create ListItem.vue

---
+++ js/components/user/ListItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="out-margin"></div>
+</template>
+
+<script>
+export default {
+  name: 'list-item',
+  data: function() {
+    return {
+      list: null,
+    }
+  },
+  props: ['id', 'login', 'email', 'fullname', 'currency', 'amount', 'banco'],
+  mounted: {
+    // ¿Donde está error? =)) 
+  }
+}
+</script>
+
+<style>
+.out-margin {
+  margin-left: 10px;
+  font-size: 14px;
+}
+</style>
