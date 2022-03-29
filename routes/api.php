284144f31b58a7a9605d14e21967be16d2ce6311
Alexander Kuziv <makklays@gmail.com>
2022-03-29 17:29:46 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,28 @@
-//
+// REST API
+// (con diferentes metodos)
 
-//
+
+// v1.0.3
 Route::name('/api/v1.0.3')->group('api', function(){
 
-  Route::post('/users', 'App\Http\Controllers\UsersController@index');
-  Route::post('/currencies', 'App\Http\Controllers\CurrenciesController@index');
-  Route::post('/bitcoins', 'App\Http\Controllers\BitcoinsController@index');
+  Route::get('/users', 'App\Http\Controllers\UsersController@index');
+  Route::post('/users/add', 'App\Http\Controllers\UsersController@add');
+  Route::put('/users/edit', 'App\Http\Controllers\UsersController@edit'); // put - edite todas datas, patch - edite part de datos
+  Route::delete('/users/delete', 'App\Http\Controllers\UsersController@delete');
 
+  Route::get('/currencies', 'App\Http\Controllers\CurrenciesController@index');
+  Route::post('/currencies/add', 'App\Http\Controllers\CurrenciesController@add');
+  Route::put('/currencies/edit', 'App\Http\Controllers\CurrenciesController@edit');
+  Route::delete('/currencies/delete', 'App\Http\Controllers\CurrenciesController@delete');
+
+  Route::get('/bitcoins', 'App\Http\Controllers\BitcoinsController@index');
+  Route::post('/bitcoins/add', 'App\Http\Controllers\BitcoinsController@add');
+  Route::put('/bitcoins/edit', 'App\Http\Controllers\BitcoinsController@edit');
+  Route::delete('/bitcoins/delete', 'App\Http\Controllers\BitcoinsController@delete');
 });
+
+// v1.0.2
+Route::name('/api/v1.0.3')->group('api', function(){
+  Route::get('/currencies', 'App\Http\Controllers\CurrenciesController@index');
+});
+
