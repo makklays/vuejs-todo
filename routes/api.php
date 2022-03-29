2a30e39ddd3205692bec99bba0ecaaf500b8ff2c
Alexander Kuziv <makklays@gmail.com>
2022-03-29 14:34:45 +0200

    Create api.php

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+//
+
+//Route::group('api')->name('/api/v1.0.3')
+
+Route::post('/users', 'App\Http\Controllers\UsersController@index');
+Route::post('/currencies', 'App\Http\Controllers\CurrenciesController@index');
+Route::post('/bitcoins', 'App\Http\Controllers\BitcoinsController@index');
+
