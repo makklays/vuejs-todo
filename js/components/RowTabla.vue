8c15ad9b1576af2f17989037b26b9bd0af76ae66
Alexander Kuziv <makklays@gmail.com>
2022-03-30 22:25:50 +0200

    Create RowTabla.vue

---
+++ js/components/RowTabla.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>row</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'row-tabla',
+  data() {
+    return {
+      items: null,
+    }
+  },
+  methods: {
+    //
+  }
+}
+</script>
+
+<style lang="scss" scope>
+.row {
+  font-size: 14px;
+}
+</style>
