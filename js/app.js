ddbc8feadb313927d2430572a05418c0319c4961
Alexander Kuziv <makklays@gmail.com>
2022-03-31 20:38:09 +0200

    2022-03-31 tabla-component' a lista de componentos

--- js/app.js
+++ js/app.js
@@ -18,6 +18,6 @@ const app = new Vue ({
     'my-componen': MyComponent,
     'user-info': UserInfoComponent,
     'list-users': ListUsers,
-    'tabla': TablaComponent,
+    'tabla-component': TablaComponent,
   }
 });
