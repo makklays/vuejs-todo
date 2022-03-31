1b01e86b8614b93f7a1badb6bc2bfd62438225b6
Alexander Kuziv <makklays@gmail.com>
2022-03-31 22:16:40 +0200

    2022-03-31 edita app.js

--- js/app.js
+++ js/app.js
@@ -8,16 +8,16 @@ window.Vue = require('vue').default;
 
 // local registration component
 //import MyComponent from "./components/MyComponent.vue";
-import UserInfoComponent from "./components/user/UserInfoComponent.vue";
-import ListUsers from "./components/user/ListUsers.vue";
+//import UserInfoComponent from "./components/user/UserInfoComponent.vue";
+//import ListUsers from "./components/user/ListUsers.vue";
 import TablaComponent from "./components/TablaComponent.vue";
 
 const app = new Vue ({
   el: '#app',
   components: {
     //'my-componen': MyComponent,
-    'user-info': UserInfoComponent,
-    'list-users': ListUsers,
+    //'user-info': UserInfoComponent,
+    //'list-users': ListUsers,
     'tabla-component': TablaComponent,
   }
 });
