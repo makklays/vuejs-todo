f989b2cce8e2b74360a2a79d88b324954a864773
Alexander Kuziv <makklays@gmail.com>
2023-05-15 11:40:53 +0200

    Update mycode.php

--- mycode.php
+++ mycode.php
@@ -36,7 +36,7 @@ class MiBoda
     $listaDeCompras = [
       '0' => 'Anillos',
       '1' => 'Floras',
-      '2' => 'Vestido', 
+      '2' => 'Vestido', // <---- ¿Tienes unas ideas?
       '3' => 'Traje', 
       '4' => '', 
     ]
